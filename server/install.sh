2a851bb667f889956a59634f6a91e2f22a4cd4b2
GitLab <greengregson@gmail.com>
2016-03-27 01:34:14 +0900

    Add useful scripts

--- server/install.sh
+++ server/install.sh
@@ -3,9 +3,9 @@
 set -eu
 
 sudo bash ./build.sh
-sudo install -m 755 ./door-controller /usr/sbin/door-controller
-sudo install -m 755 ./door-service /usr/sbin/door-service
-sudo install -m 755 ./door-rc /etc/init.d/door-rc
+sudo install -m 755 ./bin/door-controller /usr/sbin/door-controller
+sudo install -m 755 ./bin/door-service /usr/sbin/door-service
+sudo install -m 755 ./script/door-rc /etc/init.d/door-rc
 sudo update-rc.d door-rc defaults
 
 # Uninstall
