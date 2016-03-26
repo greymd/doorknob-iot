9bd95879450188e5809df905e0d3764a4ded0933
GitLab <greengregson@gmail.com>
2016-03-27 01:48:27 +0900

    Add web api files

---
+++ server/webapi/close-api.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <link rel="apple-touch-icon" href="./img/close_icon.png" />
+</head>
+<body>
+<?php
+    $output = shell_exec ('door-controller --close');
+?>
+<h1>CLOSE</h1>
+</body>
+</html>
---
+++ server/webapi/open-api.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <link rel="apple-touch-icon" href="./img/open_icon.png" />
+</head>
+<body>
+<?php
+    $output = shell_exec ('door_controller --open');
+?>
+<h1>OPEN</h1>
+</body>
+</html>
